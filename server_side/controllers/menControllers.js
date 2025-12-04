718e3128b89a8276b9165dcb291386a5c84e6bd5
rohit1620 <malavrohit1620@gmail.com>
2025-12-04 12:24:02 +0530

    add 1

--- server_side/controllers/menControllers.js
+++ server_side/controllers/menControllers.js
@@ -3,9 +3,9 @@ const Mendata=require("../models/menSchema")
 const getData=async(req,res)=>{
     try {
         const data= await Mendata.find();
-        res.status(200).json(data)
+        res.status(200).json({data})
     } catch (error) {
-        res.status(400).json({"msg":error})
+        res.status(400).json("rohit")
     }
 }
 
