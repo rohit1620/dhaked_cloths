4484ff65533d844bf89c37816a3290ed5777e71e
rohit1620 <malavrohit1620@gmail.com>
2025-12-04 13:26:06 +0530

    add 7

--- server_side/routes/womenRoute.js
+++ server_side/routes/womenRoute.js
@@ -3,6 +3,6 @@ const {getData,postData}=require("../controllers/womenControllers")
 const router=express.Router();
 
 router.route("/").get(getData);
-router.route("/").post(postData);
+router.route("/post").post(postData);
 
 module.exports=router;
