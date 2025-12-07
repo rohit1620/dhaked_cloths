7f76086988c86c9cbef0daca899be9246f3c64f7
rohit1620 <malavrohit1620@gmail.com>
2025-12-07 11:42:19 +0530

    navbar

--- client_side/dhaked_cloths/src/components/Navbar.jsx
+++ client_side/dhaked_cloths/src/components/Navbar.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
-import { faSquareWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
+import { faList } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(true);
+  const classA = "sm:hidden cursor-pointer";
+
   return (
-    <div className="max-w-full border-2 border-black border-solid  h-12 bg-black text-white flex justify-around items-center">
+    <div
+      className="max-w-full border-2  border-black border-solid  h-12 bg-black text-white flex justify-between
+      px-5 sm:px-0
+    sm:justify-around items-center relative"
+    >
       <div className="text-xl italic font-bold text-yellow-400">
         Dhaked-Cloths
       </div>
-      <ul className="flex gap-8  items-center">
+      <ul
+        className={
+          // open
+          // ? "hidden sm:block sm:flex sm:gap-8  sm:items-center"
+          // : "block sm:block sm:flex sm:gap-8  sm:items-center"
+          `${
+            open ? "hidden" : "block"
+          } sm:block sm:flex sm:gap-8  sm:items-center ${
+            !open
+              ? " fixed h-screen w-screen bg-green-600 text-white absolute top-11 left-0 flex items-center flex-col  gap-6 pt-10 transform translate-x-0 transition-transform duration-300"
+              : " fixed h-screen w-screen bg-green-600 text-white absolute top-11 left-0 flex items-center flex-col  gap-6 pt-10 transform -translate-x-full transition-transform duration-300"
+          }`
+        }
+      >
         <Link to="/" className="">
           <li>Home</li>
         </Link>
@@ -34,10 +53,11 @@ const Navbar = () => {
           <li>Singlepage</li>
         </Link> */}
       </ul>
-      <div>
-        <FontAwesomeIcon icon={faBars} className="text-xl" />
-        <FontAwesomeIcon icon={faFacebookF} />
-        <FontAwesomeIcon icon={faSquareWhatsapp} />
+      <div className="hidden sm:block">
+        <FontAwesomeIcon icon={faBasketShopping} />
+      </div>
+      <div className={classA} onClick={() => setOpen(!open)}>
+        <FontAwesomeIcon icon={faList} />
       </div>
     </div>
   );
